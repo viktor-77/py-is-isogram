0e984f2220a910008a0831fb31d659e9debb02db
viktor-77 <vumanskiy0664041341@gmail.com>
2024-07-25 13:42:32 +0300

    Resolved Is isogram

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,30 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, expected_bool_result",
+    [
+        (
+            "", True
+        ),
+        (
+            "qwerty", True
+        ),
+        (
+            "QWERTYqwerty", False
+        ),
+        (
+            "QQ", False
+        )
+        ,
+        (
+            "qq", False
+        )
+    ]
+)
+def test_is_isogram(
+    word: str, expected_bool_result: bool
+) -> None:
+    assert is_isogram(word) == expected_bool_result
